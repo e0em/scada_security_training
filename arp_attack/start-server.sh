53168d7219682c4573db44b59b2db013fdd598e0
marty@macos-vm <marty@macos-vm>
2023-04-09 09:15:48 +0000

    fix if statement error

--- arp_attack/start-server.sh
+++ arp_attack/start-server.sh
@@ -1,6 +1,10 @@
 #! /usr/bin/bash
+echo $(uname -i)
 if [ $(uname -i) = "aarch64" ]
 then
-	/usr/local/bin/docker-compose -f ./docker-compose.yml up -d
-else
+	echo "arm64"
 	/usr/local/bin/docker-compose -f ./docker-compose-arm64.yml up -d
+else
+	echo "x64"
+	/usr/local/bin/docker-compose -f ./docker-compose.yml up -d
+fi
