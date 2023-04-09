89324710bdc6774d7428e37a9a05298f50b8b0c3
Marty @ FW FDTK <marty@freedom-tek.com>
2023-04-09 17:09:38 +0800

    add arm64 判斷

--- arp_attack/start-server.sh
+++ arp_attack/start-server.sh
@@ -1,3 +1,6 @@
-
 #! /usr/bin/bash
-/usr/local/bin/docker-compose -f ./docker-compose.yml up -d
+if [ $(uname -i) = "aarch64" ]
+then
+	/usr/local/bin/docker-compose -f ./docker-compose.yml up -d
+else
+	/usr/local/bin/docker-compose -f ./docker-compose-arm64.yml up -d
